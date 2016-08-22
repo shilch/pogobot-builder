2a86f7cf62d27a6aa8892782eb74c94bac4d6ad3
shilch <sh@timetab.org>
2016-08-22 12:39:49 +0200

    Improved download (faster and more configurable)

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,11 @@
 // Polyfill
 // Array.prototype.filter
-[].filter||(Array.prototype.filter=function(a,b,c,d,e){c=this;d=[];for(e in c)~~e+''==e&&e>=0&&a.call(b,c[e],+e,c)&&d.push(c[e]);return d});
+[].filter || (Array.prototype.filter = function(a, b, c, d, e){
+  c = this;
+  d = [];
+  for(e in c)~~e + '' == e && e >= 0 && a.call(b, c[e], +e, c) && d.push(c[e]);
+  return d
+});
 
 var availableVersions = [
   '0.6.0'
@@ -13,10 +18,11 @@ if(availableVersions.indexOf(queryParamVersion) > -1){
 } else {
   botVersion = availableVersions[availableVersions.length - 1];
 }
+var baseUrl = 'data/v' + botVersion;
 
-var app = angular.module("configBuilder", [ 'ngMap', 'ngTagsInput' ]);
+var app = angular.module("configBuilder", ['ngMap', 'ngTagsInput']);
 
-app.controller("formController", function($scope, $http) {
+app.controller("formController", function($scope, $http){
   $http.get('data/v' + botVersion + '/structure.json')
     .then(function(response){
       $scope.structure = response.data;
@@ -56,7 +62,7 @@ app.controller("listController", function($scope, $http, $element){
 
   $scope.removeTag = function(tag){
     var index = tags.indexOf(tag.text);
-    if (index > -1) {
+    if(index > -1){
       tags.splice(index, 1);
     }
     updateHiddenInput();
@@ -76,68 +82,102 @@ app.controller("listController", function($scope, $http, $element){
         }).sort(function(a, b){
           return a.length - b.length;
         })
-    });
+      });
   };
 });
 
 app.controller("buttonConfigController", function($scope){
   $scope.click = function(){
-    getGeneratedConfigFile(function(err, data){
-      download('config.properties', new Blob([data], {
-        type: 'text/plain'
-      }));
+    getFileAsPlainText(baseUrl + '/config.properties.template', function(err, template){
+      template = buildConfig(template);
+      download( 'config.properties', new Blob([template], { type: 'text/plain' }) );
     });
   };
 });
 
 app.controller("buttonPackageController", function($scope){
   $scope.click = function(){
-    getGeneratedConfigFile(function(err, config){
-      getBotJar(function(err, jar){
-        var name = 'PokemonGoBot-v' + botVersion;
+    async.waterfall([
+      async.apply(getFileAsJson, baseUrl + '/content.json'),
+      function(content, done){
+        async.parallel({
+          config: async.apply(getFileAsPlainText, baseUrl + '/config.properties.template'),
+          content: async.apply(async.map, content, function(value, done){
+            getFileAsArrayBuffer(baseUrl + '/' + value, function(err, buffer){
+              if(err){
+                return done(err);
+              }
 
-        var zip = new JSZip();
-        var folder = zip.folder(name);
-        folder.file('config.properties', config);
-        folder.file('bot.jar', jar, {
+              done(null, {
+                name: value,
+                content: buffer
+              });
+            });
+          })
+        }, done)
+      }
+    ], function(err, result){
+      if(err)
+        return console.error(err);
+
+      result.config = buildConfig(result.config);
+
+      var name = 'PokemonGoBot-v' + botVersion;
+
+      var zip = new JSZip();
+      var folder = zip.folder(name);
+      folder.file('config.properties', result.config);
+      for(var j = 0; j < result.content.length; j++){
+        folder.file(result.content[j].name, result.content[j].content, {
           type: 'binary'
         });
-        folder.file('start.bat', 'java -jar bot.jar\n');
-        zip.generateAsync({ type: 'blob' })
-          .then(function(blob) {
-            download(name + '.zip', blob);
-          });
-      });
+      }
+      zip.generateAsync({ type: 'blob' })
+        .then(function(blob) {
+          download(name + '.zip', blob);
+        });
     });
   };
 });
 
-function getGeneratedConfigFile(callback){
+function buildConfig(template){
   var serialized = $('form').serializeArray();
-  $.get('data/v' + botVersion + '/config.properties.template', function(data){
-    for(var i = 0; i < serialized.length; i++){
-      var serializedPair = serialized[i];
-      data = data.replace(new RegExp(serializedPair.name + '=.*'), serializedPair.name + '=' + serializedPair.value);
-    }
-    callback(null, data);
-  });
+  for(var i = 0; i < serialized.length; i++){
+    var serializedPair = serialized[i];
+    template = template.replace(new RegExp(serializedPair.name + '=.*'), serializedPair.name + '=' + serializedPair.value);
+  }
+  return template;
 }
 
-function getBotJar(callback){
+function getFileAsArrayBuffer(url, callback){
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'data/v' + botVersion + '/bot.jar', true);
+  xhr.open('GET', url, true);
   xhr.responseType = 'arraybuffer';
 
-  xhr.onload = function(e) {
-    if (xhr.status == 200) {
+  xhr.onload = function(err){
+    if(xhr.status == 200){
       callback(null, new Uint8Array(xhr.response));
+    } else {
+      callback(err);
     }
   };
 
   xhr.send();
 }
 
-function download(filename, blob) {
+function getFileAsJson(url, callback){
+  $.getJSON(url, function(json){
+    callback(null, json);
+  });
+}
+
+function getFileAsPlainText(url, callback){
+  $.get(url, function(text){
+    callback(null, text);
+  });
+}
+
+function download(filename, blob){
   var element = document.createElement('a');
   element.setAttribute('href', URL.createObjectURL(blob));
   element.setAttribute('download', filename);
@@ -150,12 +190,12 @@ function download(filename, blob) {
   document.body.removeChild(element);
 }
 
-function getParameterByName(name, url) {
-  if (!url) url = window.location.href;
+function getParameterByName(name, url){
+  if(!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, "\\$&");
   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
     results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return '';
+  if(!results) return null;
+  if(!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
